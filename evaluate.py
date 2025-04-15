58fb05759982bf80d7596aae4edea1bea07dd3e6
Mackenzie van Zanden <macktvz@gmail.com>
2025-04-15 18:34:21 -0400

    Working experiments, working evaluation, working pixelsmith code

--- evaluate.py
+++ evaluate.py
@@ -1,56 +1,83 @@
-from torcheval.metrics import FrechetInceptionDistance
-from torchmetrics.functional.multimodal.clip_score import CLIPScore
+from torchmetrics.image.fid import FrechetInceptionDistance
+from torchmetrics.multimodal.clip_score import CLIPScore
 from torchmetrics.image.kid import KernelInceptionDistance
-#from torchmetrics.image.inception import InceptionScore
+from torchmetrics.image.inception import InceptionScore
 
-from reproducibility_project.utils import load_data, load_gen_data
+from reproducibility_project.utils import load_dataset, load_gen_data
 from torch import Tensor
-
+import torch
+from torchvision.transforms.functional import pil_to_tensor
+from torchvision.transforms import Resize
 # TODO: Think about moving stuff to gpu
 
+
+EVAL_N = 1000
+DESIRED_RES = 2048
 def load_tensors():
-    real_data = load_data()
+    real_data = load_dataset()
     gen_data = load_gen_data()
 
     # want two tensors, lined up
-    REAL_IMAGE_KEY = "image"
     real_tensors = [
-        d[REAL_IMAGE_KEY]
-        for d in real_data
+        t
+        for t in real_data["tensor"]
     ]
     
     FAKE_IMAGE_KEY = "img"
     fake_tensors = [
-        gen_data[i][FAKE_IMAGE_KEY]
-        for i in range(len(real_data))
+        pil_to_tensor([d["img"] for d in gen_data if int(d["idx"]) == i and int(d["res"]) == DESIRED_RES][0])
+        for i in range(len(real_data["url"]))
     ]
 
-    CAPTION_KEY = "text"
+    CAPTION_KEY = "caption"
     captions = [
-        d[CAPTION_KEY]
-        for d in real_data
+        c
+        for c in real_data[CAPTION_KEY]
     ]
+    # make sure these mfs r the right size
+    good_inds = [i for i in range(len(real_tensors)) if real_tensors[i].shape[0] == 3]
+
+    real_tensors = [real_tensors[i] for i in good_inds]
+    fake_tensors = [fake_tensors[i] for i in good_inds if i < len(fake_tensors)]
+    captions = [captions[i] for i in good_inds]
 
     # TODO: COnvert to torch tensors
-
-    return real_tensors, fake_tensors, captions
+    sz = min(EVAL_N, len(fake_tensors))
+    return real_tensors[:sz], fake_tensors[:sz], captions[:sz]
 
 real_tensors, fake_tensors, captions = load_tensors()
 N = len(real_tensors)
 
 def calc_frechet():
-    BATCH_SIZE = 8
+    rsz = Resize((299,299))
+    
+    rt = torch.stack([rsz.forward(t) for t in real_tensors])
+    ft = torch.stack([rsz.forward(t) for t in fake_tensors])
+    BATCH_SIZE = 32
     frech = FrechetInceptionDistance()
     for k in range(N // BATCH_SIZE + 1):
-        frech.update(real_tensors[k*BATCH_SIZE:(k+1)*BATCH_SIZE], is_real=True)
-        frech.update(fake_tensors[k*BATCH_SIZE:(k+1)*BATCH_SIZE], is_real=False)
+        frech.update(rt[k*BATCH_SIZE:(k+1)*BATCH_SIZE], real=True)
+        frech.update(ft[k*BATCH_SIZE:(k+1)*BATCH_SIZE], real=False)
+        print(f"processed {(k+1)*32} imgs")
+    
+    return frech.compute()
+
+def calc_is():
+    rsz = Resize((299,299))
+    
+    rt = torch.stack([rsz.forward(t) for t in real_tensors]).to(torch.uint8)
+    ft = torch.stack([rsz.forward(t) for t in fake_tensors]).to(torch.uint8)
+    BATCH_SIZE = 32
+    frech = InceptionScore()
+    for k in range(N // BATCH_SIZE + 1):
+        frech.update(ft[k*BATCH_SIZE:(k+1)*BATCH_SIZE])
     
     return frech.compute()
 
 def calc_clip():
     MODEL = "openai/clip-vit-base-patch16"
     clip = CLIPScore(MODEL)
-    BATCH_SIZE = 8
+    BATCH_SIZE = 32
     for k in range(N // BATCH_SIZE + 1):
         start, end = k*BATCH_SIZE, (k+1)*BATCH_SIZE
         clip.update(captions[start:end], fake_tensors[start:end])
@@ -58,16 +85,34 @@ def calc_clip():
     return clip.compute()
 
 def calc_kid():
-    BATCH_SIZE = 8
-    kid = KernelInceptionDistance()
+    rsz = Resize((299,299))
+
+    rt = torch.stack([rsz.forward(t) for t in real_tensors]).to(torch.uint8)
+    ft = torch.stack([rsz.forward(t) for t in fake_tensors]).to(torch.uint8)
+    BATCH_SIZE = 32
+    kid = KernelInceptionDistance(subset_size=50)
     for k in range(N // BATCH_SIZE + 1):
-        kid.update(real_tensors[k*BATCH_SIZE:(k+1)*BATCH_SIZE], real=True)
-        kid.update(fake_tensors[k*BATCH_SIZE:(k+1)*BATCH_SIZE], real=False)
+        kid.update(rt[k*BATCH_SIZE:(k+1)*BATCH_SIZE], real=True)
+        kid.update(ft[k*BATCH_SIZE:(k+1)*BATCH_SIZE], real=False)
     
     return kid.compute()
 
+from pathlib import Path
+import pickle
+
+
+SAVE_PATH = Path(__file__).parent / "eval"
+SAVE_PATH.mkdir(exist_ok=True)
+def save_eval(f, c, k, i, n=EVAL_N):
+    f_name = SAVE_PATH / f"{n}.pkl"
+    with f_name.open("wb") as fl:
+        pickle.dump({"frechet":f, "clip":c, "kernel":k, "n":n, "i": i}, fl)
+
 if __name__ == "__main__":
-    print("FRECHET SCORE: ", calc_frechet())
-    print("CLIP SCORE: ", calc_clip())
-    print("KID SCORE: ", calc_kid())
+    print(f"EVALUATING {len(fake_tensors)} TENSORS")
+    print("FRECHET SCORE: ", f := calc_frechet())
+    print("CLIP SCORE: ", c := calc_clip())
+    print("KID SCORE: ", k := calc_kid())
+    print("I SCORE: ", i := calc_is())
+    save_eval(f, c, k, i)
 
--- run_experiments.py
+++ run_experiments.py
@@ -1,14 +1,15 @@
-from pipeline import generate_image, Pixelsmith, AutoencoderKL, DDIMScheduler
-from reproducibility_project.utils import load_data
-from reproducibility_project.config import DATA_PATH
+from pipeline import Pixelsmith, AutoencoderKL, DDIMScheduler
+from reproducibility_project.utils import load_dataset
+from reproducibility_project.config import DATA_PATH, GEN_IMGS_PATH
 import torch
 import pickle
 
-seed = 1
-generator = torch.manual(seed)
-# list of dicts with keys ["image", "caption", "width", "height"]
-data = load_data()
 
+seed = 1
+generator = torch.manual_seed(seed)
+# list of dicts with keys ["image", "caption", "width", "height"]
+data = load_dataset()
+N = len(data["caption"])
 resolutions = [
     2048,
     4096
@@ -16,12 +17,10 @@ resolutions = [
 
 batch_size = 1
 
-SAVE_FOLDER = DATA_PATH / "generated_imgs"
-SAVE_FOLDER.mkdir(exist_ok=True)
-already_generated = [p.name for p in SAVE_FOLDER.glob("*")]
-
-vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16).to("cuda")
-pixelsmith = Pixelsmith.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0",torch_dtype=torch.float16,vae=vae).to("cuda")
+already_generated = [p.name for p in GEN_IMGS_PATH.glob("*")]
+DEVICE = "cuda"
+vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16).to(DEVICE)
+pixelsmith = Pixelsmith.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0",torch_dtype=torch.float16,vae=vae).to(DEVICE)
 pixelsmith.scheduler = DDIMScheduler.from_config(pixelsmith.scheduler.config, set_alpha_to_one=True)
 
 def gen_images(prompts, res):
@@ -43,16 +42,17 @@ def gen_file_name(idx, res):
     return f"{idx}_{res}.pkl"
 
 def save_data(img, idx, res):
-    with (SAVE_FOLDER / gen_file_name(idx, res)).open('wb') as f:
+    with (GEN_IMGS_PATH / gen_file_name(idx, res)).open('wb') as f:
         pickle.dump(make_gen_img_data(img, idx, res), f)
 # what format to save in?
 
-for i in (len(data) // batch_size + 1):
-    idxs = range(i*batch_size, (i+1)*batch_size)
-    
-    for r in resolutions:
+for r in resolutions:
+    for i in range(N // batch_size + 1):
+        idxs = range(i*batch_size, (i+1)*batch_size)
+        
         to_gen_idx = [idx for idx in idxs if gen_file_name(idx, r) not in already_generated]
-        prompts = [data[i]["caption"] for i in to_gen_idx]
+        if not to_gen_idx: continue
+        prompts = [data["caption"][i] for i in to_gen_idx]
         batch_images = gen_images(prompts, r)
         # format and save data
         for img, idx in zip(batch_images, to_gen_idx):
